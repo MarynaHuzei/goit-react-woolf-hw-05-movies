b2fcc1b62d13147e95c2f209a5077fef45761e70
MarznaHuzei <marinka777xx@gmail.com>
2024-03-03 23:05:05 +0100

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,9 +14,10 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MovieDetails />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
-        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/movies/:movieId/cast" element={<Cast />} />
+          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );
